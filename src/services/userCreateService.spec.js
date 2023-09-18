275785876bcd453019245ed6f6e4a37e78fc5cff
Adriel Klem <adrielklem16@gmail.com>
2023-09-18 19:17:19 -0300

    add secondary test UserServices

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,18 +1,41 @@
 const UserCreateService = require("./UserCreateService")
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError");
 
-it("user should be create", async () => {
-    const user = {
-        name: "User Test",
-        email: "user@test.com",
-        password: "123"
-    }
+describe("UserCreateService", () => {
+    it("user should be create", async () => {
+        const user = {
+            name: "User Test",
+            email: "user@test.com",
+            password: "123"
+        }
+        
+        const userRepositoryInMemory = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepositoryInMemory)
+        const userCreated = await userCreateService.execute(user)
+
+        console.log(userCreated)
+
+        expect(userCreated).toHaveProperty("id")
+    }),
     
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
-    const userCreated = await userCreateService.execute(user)
+    it("user not should be create with exists email", async () => {
+        const user1 = {
+            name: "User Test 1",
+            email: "user@test.com",
+            password: "123"
+        }
 
-    console.log(userCreated)
+        const user2 = {
+            name: "User Test 2",
+            email: "user@test.com",
+            password: "456"
+        }
 
-    expect(userCreated).toHaveProperty("id")
+        const userRepositoryInMemory = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepositoryInMemory)
+
+        await userCreateService.execute(user1) 
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."))
+    })
 })
